22770777e979353dc7c8c2a4cb9a560e46e6a0b7
OM BHARGAV <ombhargav788@gmail.com>
2026-02-28 21:48:10 +0530

    updated route

--- app/api/admin/users/[id]/route.ts
+++ app/api/admin/users/[id]/route.ts
@@ -97,17 +97,18 @@ export async function PUT(
         { status: 400 }
       );
     }
-    const exist = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (exist) {
-      return NextResponse.json(
-        { success: false, message: "Email Already Exist!" },
-        { status: 400 }
-      );
-    }
+
+    // const exist = await prisma.user.findUnique({
+    //   where: {
+    //     email,
+    //   },
+    // });
+    // if (exist && exist.email!==email) {
+    //   return NextResponse.json(
+    //     { success: false, message: "Email Already Exist!" },
+    //     { status: 400 }
+    //   );
+    // }
     const updateData: any = {};
 
     if (status) {
